a136224e83d6233be323f4ce921f9f61826695e7
Srihari <127367440+Sdasarisrihari@users.noreply.github.com>
2024-09-30 20:09:13 -0400

    updated files

---
+++ Docker
@@ -0,0 +1,14 @@
+# Use the official PHP image with Apache
+FROM php:8.0-apache
+
+# Install PHP extensions required for your project
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+
+# Copy your project files into the container's web directory
+COPY . /var/www/html/
+
+# Set correct permissions for the Apache web server to access your files
+RUN chown -R www-data:www-data /var/www/html
+
+# Expose port 80 to allow the container to serve the web app
+EXPOSE 80
--- create_database.php
+++ create_database.php
@@ -1,101 +1,54 @@
+<!-- This file is just used to create all the required database and tables. -->
 <?php
-// Database connection parameters
-$dbHost = 'db'; // Use the service name defined in docker-compose.yml
-$dbUser = 'root'; // MySQL root username
-$dbPass = 'root'; // MySQL root password
-$dbName = 'easymeds'; // Database name to use
 
-// Create connection
-$con = mysqli_connect($dbHost, $dbUser, $dbPass) or die("Unable to connect");
+$con = mysqli_connect("localhost","root","") or die("Unable to connet");
+$sql = 'CREATE Database easymeds';
+$retval = mysqli_query($con,$sql);
 
-// Create Database if it doesn't exist
-$sql = "CREATE DATABASE IF NOT EXISTS $dbName";
-$retval = mysqli_query($con, $sql);
-
-if ($retval) {
-    echo "<script>alert('Database Created Successfully')</script>";
-} else {
-    echo "<script>alert('Database already exists')</script>";
+if($retval){
+	echo "<script>alert('Database Created Sucessfully')</script>";
+}
+else{
+	echo "<script>alert('Database already exists')</script>";
 }
 
-// Select the database
-mysqli_select_db($con, $dbName);
+$query = "CREATE TABLE `easymeds`.`login` (`uid` VARCHAR(10),PRIMARY KEY (`uid`),`uname` VARCHAR(30) NOT NULL, `password` VARCHAR(30) NOT NULL ,`full_name` VARCHAR(100) NOT NULL , `role` VARCHAR(20) NOT NULL , `email` VARCHAR(100) NOT NULL , `phone_no` VARCHAR(15) NOT NULL,`address` VARCHAR(200) NOT NULL,`gender` VARCHAR(50) NOT NULL)";
+$query_run = mysqli_query($con,$query);
 
-// Create login table
-$query = "CREATE TABLE IF NOT EXISTS `login` (
-    `uid` VARCHAR(10) PRIMARY KEY,
-    `uname` VARCHAR(30) NOT NULL,
-    `password` VARCHAR(30) NOT NULL,
-    `full_name` VARCHAR(100) NOT NULL,
-    `role` VARCHAR(20) NOT NULL,
-    `email` VARCHAR(100) NOT NULL,
-    `phone_no` VARCHAR(15) NOT NULL,
-    `address` VARCHAR(200) NOT NULL,
-    `gender` VARCHAR(50) NOT NULL
-)";
-$query_run = mysqli_query($con, $query);
+if($query_run){
+	echo "<script>alert('Created Sucessfully Login Table')</script>";
+}
+else{
+	echo "<script>alert('Tables already exists')</script>";
+}
+$query = "CREATE TABLE `easymeds`.`stock` ( `stock_id` VARCHAR(20) NOT NULL ,`uid` VARCHAR(20) NOT NULL ,`medicine_name` VARCHAR(100) NOT NULL , `manufacturer_name` VARCHAR(100) NOT NULL , `stock_quantity` VARCHAR(10) NOT NULL , `value` VARCHAR(15) NOT NULL , `date_of_expiry` VARCHAR(20) NOT NULL , PRIMARY KEY (`stock_id`))";
+$query_run = mysqli_query($con,$query);
 
-if ($query_run) {
-    echo "<script>alert('Created Successfully Login Table')</script>";
-} else {
-    echo "<script>alert('Login Table already exists')</script>";
+if($query_run){
+	echo "<script>alert('Created Sucessfully stock table')</script>";
+}
+else{
+	echo "<script>alert('Tables already exists')</script>";
 }
 
-// Create stock table
-$query = "CREATE TABLE IF NOT EXISTS `stock` (
-    `stock_id` VARCHAR(20) NOT NULL PRIMARY KEY,
-    `uid` VARCHAR(20) NOT NULL,
-    `medicine_name` VARCHAR(100) NOT NULL,
-    `manufacturer_name` VARCHAR(100) NOT NULL,
-    `stock_quantity` VARCHAR(10) NOT NULL,
-    `value` VARCHAR(15) NOT NULL,
-    `date_of_expiry` VARCHAR(20) NOT NULL
-)";
-$query_run = mysqli_query($con, $query);
+$query = "CREATE TABLE `easymeds`.`cart` ( `uid` VARCHAR(20) NOT NULL, `seller_uid` VARCHAR(20) NOT NULL,`stock_id` VARCHAR(20) NOT NULL ,`medicine_name` VARCHAR(100) NOT NULL ,`quantity` VARCHAR(10) NOT NULL, `price` VARCHAR(10) NOT NULL)";
+$query_run = mysqli_query($con,$query);
 
-if ($query_run) {
-    echo "<script>alert('Created Successfully stock table')</script>";
-} else {
-    echo "<script>alert('Stock Table already exists')</script>";
+if($query_run){
+	echo "<script>alert('Created Sucessfully cart table')</script>";
+}
+else{
+	echo "<script>alert('Table already exists')</script>";
 }
 
-// Create cart table
-$query = "CREATE TABLE IF NOT EXISTS `cart` (
-    `uid` VARCHAR(20) NOT NULL,
-    `seller_uid` VARCHAR(20) NOT NULL,
-    `stock_id` VARCHAR(20) NOT NULL,
-    `medicine_name` VARCHAR(100) NOT NULL,
-    `quantity` VARCHAR(10) NOT NULL,
-    `price` VARCHAR(10) NOT NULL
-)";
-$query_run = mysqli_query($con, $query);
+$query= "CREATE TABLE `easymeds`.`billing` ( `billing_no` VARCHAR(20) NOT NULL ,  `uid` VARCHAR(50) NOT NULL , `seller_uid` VARCHAR(20) NOT NULL, `stock_id` VARCHAR(20) NOT NULL , `date` VARCHAR(20) NOT NULL, `medicine_name` VARCHAR(100) NOT NULL,`quantity` VARCHAR(10) NOT NULL , `price` VARCHAR(10) NOT NULL, `indiviual_cost` VARCHAR(20) NOT NULL )";
+$query_run = mysqli_query($con,$query);
 
-if ($query_run) {
-    echo "<script>alert('Created Successfully cart table')</script>";
-} else {
-    echo "<script>alert('Cart Table already exists')</script>";
+if($query_run){
+	echo "<script>alert('Created Sucessfully billing table')</script>";
+}
+else{
+	echo "<script>alert('Table already exists')</script>";
 }
 
-// Create billing table
-$query = "CREATE TABLE IF NOT EXISTS `billing` (
-    `billing_no` VARCHAR(20) NOT NULL,
-    `uid` VARCHAR(50) NOT NULL,
-    `seller_uid` VARCHAR(20) NOT NULL,
-    `stock_id` VARCHAR(20) NOT NULL,
-    `date` VARCHAR(20) NOT NULL,
-    `medicine_name` VARCHAR(100) NOT NULL,
-    `quantity` VARCHAR(10) NOT NULL,
-    `price` VARCHAR(10) NOT NULL,
-    `individual_cost` VARCHAR(20) NOT NULL
-)";
-$query_run = mysqli_query($con, $query);
-
-if ($query_run) {
-    echo "<script>alert('Created Successfully billing table')</script>";
-} else {
-    echo "<script>alert('Billing Table already exists')</script>";
-}
-
-// Close the database connection
-mysqli_close($con);
 ?>
